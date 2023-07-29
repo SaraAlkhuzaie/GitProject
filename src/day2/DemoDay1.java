18daed49110f1120fd9d8ff24cb318d8ac8f1fec
saifA <saifA@Saber.myfiosgateway.com>
2023-07-29 19:53:02 -0400

    implemented new method

--- src/day2/DemoDay1.java
+++ src/day2/DemoDay1.java
@@ -4,5 +4,9 @@ public class DemoDay1 {
 	public static void main(String[] args) {
 		System.out.println("test");
 	}
-
+public static void add (int a, int b) {
+	System.out.println(a+b);
+}
+public static void subtract (int a, int b) {
+	System.out.println(a-b);
 }
